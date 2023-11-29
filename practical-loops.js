8d8ef2ddfcff49bd6b57ff4fe991186a2c65a018
Leon.Code <gregory.requena@gmail.com>
2023-11-29 18:10:22 -0500

    Updated Practical Loops

--- practical-loops.js
+++ practical-loops.js
@@ -31,11 +31,40 @@ while (true) {
 }
 
 //Part 3: Feeling Loopy
-let newString = 'Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232'
+let newString = "ID , NAME , OCCUPATION, AGE;\n42, Bruce, Knight, 41 \n57, Bob, Fry Cook, 19 \n63, Blaine, Quiz Master,58 \n98, Bill, Doctor's Assistant, 26"
+
+let cell1 = "";
+let cell2 = "";
+let cell3 = "";
+let cell4 = "";
 
 let rows = newString.split("\n");
 
 for (let i = 0; i < rows.length; i++){
+    
     let cells = rows[i].split(",");
-    console.log(cells[0], cells[1], cells[2], cells[3]);
+    cell1 += cells[0] + " ";
+    cell2 += cells[1] + " ";
+    cell3 += cells[2] + " ";
+    cell4 += cells[3] + " ";
+    console.log(cells);
+}
+
+let newString2 = "Index, Mass (kg), Spring 1 (m), Spring 2 (m);\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232"
+
+let cell5 = "";
+let cell6 = "";
+let cell7 = "";
+let cell8 = "";
+
+let rows2 = newString2.split("\n");
+
+for (let i = 0; i < rows2.length; i++) {
+
+    let cells = rows2[i].split(",");
+    cell1 += cells[5] + " ";
+    cell2 += cells[6] + " ";
+    cell3 += cells[7] + " ";
+    cell4 += cells[8] + " ";
+    console.log(cells);
 }
